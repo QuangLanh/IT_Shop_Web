f0e87484029b6060296d7ead4d9261cebcf329b7
QuangLanh <tpsf.q.lanh@gmail.com>
2022-05-16 09:24:44 +0700

    update

---
+++ models/OrderModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const orderSchema = new Schema({
+    id: { type: ObjectId },
+    address: { type: String },
+    totalmoney: { type: Number },
+    productQuantity: { type: Number },
+    productArr:{type: Array},
+    published:{type:Date},
+    userid:{type: ObjectId},
+    status: {type: String}
+});
+
+module.exports = mongoose.model('order', orderSchema);
